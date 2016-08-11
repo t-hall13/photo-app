c3d935e793ecc77b6243e933149ead39246f9500
Tim Hall <timothypatrickhall@gmail.com>
2016-08-11 02:18:48 +0000

    fixed typo in image.rb

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,7 +5,7 @@ class Image < ActiveRecord::Base
   
   private
   def picture_size
-    if picture_size > 5.megabytes
+    if picture.size > 5.megabytes
       errors.add(:picture, "should be less than 5MB") 
     end
   end
